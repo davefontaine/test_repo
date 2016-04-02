7c1a9fa3ca45017384bcb5ea7ee77d4ff7b389c0
Dave Fontaine <dfontaine@linkedin.com>
2016-04-02 13:38:40 -0700

    bug fix

--- csv_reader.py
+++ csv_reader.py
@@ -6,5 +6,6 @@ import numpy as np
 print np.recfromcsv(sys.argv.pop())
 print "new branch for testing"
 print "testing if I modify file but do not git add, only git commit"
+print "I fixed this bug!!! dev created already"
 
 
